762cf9728cc5052aade36969cd6555f5b1ae776b
badyanov <d.badyanov@outlook.com>
2022-03-04 17:06:58 +0300

    part 5 - жизненные циклы и ErrorBoundary

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from "react";
 import Car from "./Car/Car";
+import ErrorBoundary from "./ErrorBoundary/ErrorBoundary";
 import "./App.css";
 
 class App extends React.Component {
-  state = {
-    cars: [
-      { name: "Ford", year: 2018 },
-      { name: "Audi", year: 2021 },
-      { name: "Mazda", year: 2011 },
-    ],
-    pageTitle: '*',
-    showCars: false
-  };
+
+  constructor(props) {
+    console.log('App constructor');
+
+    super(props);
+
+    this.state = {
+      cars: [
+        { name: "Ford", year: 2018 },
+        { name: "Audi", year: 2021 },
+        { name: "Mazda", year: 2011 },
+      ],
+      pageTitle: '*',
+      showCars: false
+    };
+  }
 
   onChangeName(name, index) {
     const car = this.state.cars[index];
@@ -42,7 +50,17 @@ class App extends React.Component {
     this.setState({cars})
   }
 
+  // componentWillMount() {
+  //   console.log('App componentWillMount');
+  // }
+
+  componentDidMount() {
+    console.log('App componentDidMount')
+  }
+  
   render() {
+    console.log('App render');
+
     const divStyle = {
       textAlign: "center",
     };
@@ -50,20 +68,26 @@ class App extends React.Component {
     let cars = null;
     if (this.state.showCars) {
       cars = this.state.cars.map((car, index) => {
-        return (<Car 
+        return (
+        <ErrorBoundary>
+          <Car 
           key={index}
           name={car.name} 
           year={car.year} 
           onDelete={this.deleteHandler.bind(this, index)}
           onChangeName={(event) => this.onChangeName(event.target.value, index)}
-        />)
+          />
+        </ErrorBoundary>
+        )
       })
     }
 
     return (
       <div style={divStyle}>
-        <h1>React components</h1>
-        <h2>{this.state.pageTitle}</h2>
+        {/* <h1>React components</h1> */}
+        {/* <h2>{this.state.pageTitle}</h2> */}
+
+        <h1>{this.props.title}</h1>
 
         <button onClick={this.toggleCarsHandler}>Show cars</button>
         <div style={{
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,28 +1,70 @@
 import React from "react";
 import './Car.css'
 
-export default (props) => {
-  const inputClasses = ['input'];
- 
-  if (props.name !== '') {
-    inputClasses.push('green');
-  } else {
-    inputClasses.push('red');
+class Car extends React.Component {
+
+  // UNSAFE_componentWillReceiveProps(nextProps) {
+  //   console.log('Car UNSAFE_componentWillReceiveProps', nextProps);
+  // }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('Car shouldComponentUpdate', nextProps, nextState);
+    return nextProps.name.trim() !== this.props.name.trim();
   }
 
-  if (props.name.length > 4) {
-    inputClasses.push('bold');
+  // componentWillUpdate(nextProps, nextState) {
+  //   console.log('Car componentWillUpdate', nextProps, nextState);
+  // }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    console.log('Car getDerivedStateFromProps', nextProps, prevState);
+
+    return prevState;
   }
 
-  return (
-  <div className="Car">
-    <h3>Car name: {props.name}</h3>
-    <p>Year: {props.year}</p>
-    <input 
-      className={inputClasses.join(' ')}
-      type='text' 
-      onChange={props.onChangeName} 
-      value={props.name}/>
-    <button onClick={props.onDelete}>Delete</button>
-  </div>
-)}
+  componentDidUpdate() {
+    console.log('Car componentDidUpdate');
+  }
+
+  getSnapshotBeforeUpdate() {
+    console.log('Car getSnapshotBeforeUpdate');
+  }
+
+  componentWillUnmount() {
+    console.log('Car componentWillUnmount');
+  }
+
+  render() {
+    console.log('Car render');
+
+    if (Math.random() > 0.7) {
+      throw new Error('Car random failed');
+    }
+
+    const inputClasses = ['input'];
+   
+    if (this.props.name !== '') {
+      inputClasses.push('green');
+    } else {
+      inputClasses.push('red');
+    }
+  
+    if (this.props.name.length > 4) {
+      inputClasses.push('bold');
+    }
+  
+    return (
+    <div className="Car">
+      <h3>Car name: {this.props.name}</h3>
+      <p>Year: {this.props.year}</p>
+      <input 
+        className={inputClasses.join(' ')}
+        type='text' 
+        onChange={this.props.onChangeName} 
+        value={this.props.name}/>
+      <button onClick={this.props.onDelete}>Delete</button>
+    </div>
+  )} 
+}
+
+export default Car;
